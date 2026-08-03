33930bc101db5a47f9db33885c6afdc70b777465
zachspang <spangzach@gmail.com>
2026-08-02 18:42:53 -0700

    Add ViGEmClient submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <Xinput.h>
 #include <wx/wx.h>
+#include <ViGEm/Client.h>
 
 class MyApp : public wxApp {
 public:
@@ -20,8 +24,46 @@ enum {
 };
 
 wxIMPLEMENT_APP(MyApp);
+PVIGEM_CLIENT client;
+PVIGEM_TARGET pad;
 
 bool MyApp::OnInit() {
+    // vigemclient test
+
+    client = vigem_alloc();
+
+    if (client == nullptr)
+    {
+        std::cerr << "Uh, not enough memory to do that?!" << std::endl;
+        return -1;
+    }
+    const auto retval = vigem_connect(client);
+
+    if (!VIGEM_SUCCESS(retval))
+    {
+        std::cerr << "ViGEm Bus connection failed with error code: 0x" << std::hex << retval << std::endl;
+        return -1;
+    }
+
+    //
+    // Allocate handle to identify new pad
+    //
+    pad = vigem_target_x360_alloc();
+
+    //
+    // Add client to the bus, this equals a plug-in event
+    //
+    const auto pir = vigem_target_add(client, pad);
+
+    //
+    // Error handling
+    //
+    if (!VIGEM_SUCCESS(pir))
+    {
+        std::cerr << "Target plugin failed with error code: 0x" << std::hex << pir << std::endl;
+        return -1;
+    }
+
     MyFrame* frame = new MyFrame();
     frame->Show(true);
     return true;
@@ -53,6 +95,8 @@ MyFrame::MyFrame()
 }
 
 void MyFrame::OnExit(wxCommandEvent& event) {
+    vigem_target_remove(client, pad);
+    vigem_target_free(pad);
     Close(true);
 }
 
@@ -62,5 +106,18 @@ void MyFrame::OnAbout(wxCommandEvent& event) {
 }
 
 void MyFrame::OnHello(wxCommandEvent& event) {
-    wxLogMessage("Hello world from wxWidgets!");
+    XINPUT_STATE state;
+
+    XInputGetState(0, &state);
+
+    XUSB_REPORT report;
+    report.wButtons = XINPUT_GAMEPAD_A | XINPUT_GAMEPAD_DPAD_LEFT;
+    report.sThumbLX = 0;
+    report.sThumbLY = 0;
+    report.sThumbRX = 0;
+    report.sThumbRY = 0;
+    report.bLeftTrigger = 0;
+    report.bRightTrigger = 0;
+
+    vigem_target_x360_update(client, pad, report);
 }
